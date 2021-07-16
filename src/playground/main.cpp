31dbaf11321f45fda48853b6ab6ede5c5f2e7e27
Lukas Turcani <lukasturcani93@gmail.com>
2021-07-16 09:16:36 +0100

    Add binary

---
+++ src/playground/main.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+
+/*
+#include "src/playground/Vector3.h"
+#include "src/playground/dod/PhysicsBody.h"
+#include "src/playground/dod/PhysicsSystem/PhysicsSystem.h"
+*/
+
+
+auto main() -> int
+{
+    /*
+    std::vector<PhysicsBody> bodies {
+        PhysicsBody {
+            Vector3 { 0.f, 0.f, 0.f },
+            Vector3 { 0.f, 0.f, 0.f },
+        },
+        PhysicsBody {
+            Vector3 { 0.f, 0.f, 0.f },
+            Vector3 { 0.f, 0.f, 0.f },
+        },
+    };
+    const PhysicsSystem physics_system {};
+    const PhysicsConfig physics_config {
+        Seconds { 1.f },
+    };
+    physics_system.update_bodies(physics_config, bodies);
+    */
+    return EXIT_SUCCESS;
+}
